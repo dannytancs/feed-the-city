cd053a652bd1dba2062397fa0a875479dd14030c
Danny Tan <dannytan.ce@gmail.com>
2018-12-09 15:58:44 -0500

    current coordinate

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { withAuthorization } from '../Session';
 
-const HomePage = () => (
-  <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-  </div>
-);
+
+
+
+class HomePage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      lat: null,
+      lon: null,
+      loading: true
+    }
+  }
+
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        loading: false
+      })
+    })
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        {this.state.loading && <div>Loading ...</div>}
+        {!this.state.loading &&
+          <div>
+            <div>
+              Latitude: {this.state.lat}
+            </div>
+            <div>
+              Longitude: {this.state.lon}
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+
 
 const condition = authUser => !!authUser;
 
