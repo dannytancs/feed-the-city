f795cd2316f1251e49d6959f9bc01ff7619b00c7
Danny Tan <dannytan.ce@gmail.com>
2018-12-10 19:10:43 -0500

    multiple markers

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import { AuthUserContext, withAuthorization } from '../Session';
 import { FirebaseContext } from '../Firebase';
 
 
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 
 // class Shelter extends Component {
@@ -31,9 +31,13 @@ class HomePageBase extends Component {
       shelters: [],
       sheltersCoordinates: [],
       selectedOption: null,
-      submitRestaurant: false
+      chosenRestaurant: null,
+      chosenShelter: null,
+      submitRestaurant: false,
+      submitShelter: false
     }
-    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleOptionChangeRestaurant = this.handleOptionChangeRestaurant.bind(this);
+    this.handleOptionChangeShelter = this.handleOptionChangeShelter.bind(this);
   }
 
 
@@ -72,18 +76,31 @@ class HomePageBase extends Component {
   };
 
 
-  handleOptionChange(changeEvent) {
+  handleOptionChangeRestaurant(changeEvent) {
     this.setState({
-      selectedOption: changeEvent.target.value
+      selectedOption: changeEvent.target.value,
+      chosenRestaurant: changeEvent.target.value
     });
-    console.log(changeEvent.target.value)
+  }
+
+  handleOptionChangeShelter(changeEvent) {
+    this.setState({
+      selectedOption: changeEvent.target.value,
+      chosenShelter: changeEvent.target.value
+    });
   }
 
 
   onSubmit = event => {
     this.setState({
-      submitRestaurant: true
+      submitRestaurant: true,
     });
+  }
+
+  onSubmitShelter = event => {
+    this.setState({
+      submitShelter: true
+    })
   }
 
 
@@ -98,70 +115,101 @@ class HomePageBase extends Component {
           <form onSubmit={this.onSubmit}>
             {Object.keys(this.state.restaurants).map(key => (
 
-              <div className="radio" key={key}>
-                <label>
-                  <input type="radio" value={key} checked={this.state.selectedOption === key} onChange={this.handleOptionChange} />
-                  <div>
-                    <strong>Name:</strong> {this.state.restaurants[key].name}
-                  </div>
-                  <div>
-                    <strong>Address:</strong> {this.state.restaurants[key].address}
-                  </div>
-                  <div>
-                    <strong>Phone:</strong> {this.state.restaurants[key].phone}
-                  </div>
 
-                </label>
-              </div>
+              <label key={key} value={key}>
+                <input type="radio" value={key} checked={this.state.selectedOption === key} onChange={this.handleOptionChangeRestaurant} />
+                <div>
+                  <strong>Name:</strong> {this.state.restaurants[key].name}
+                </div>
+                <div>
+                  <strong>Address:</strong> {this.state.restaurants[key].address}
+                </div>
+                <div>
+                  <strong>Phone:</strong> {this.state.restaurants[key].phone}
+                </div>
+              </label>
+
             ))}
 
             <button className="btn btn-primary" style={{ right: "52%", position: "absolute", top: "13%" }} type="submit">
               Select
                 </button>
-
           </form>
         </div>
         }
         {
-          !this.state.loading && !this.state.submitRestaurant && <GoogleApiRestaurant bounds={this.state.bounds} lat={this.state.lat} lon={this.state.lon} />
+          !this.state.loading && !this.state.submitRestaurant && <GoogleApiRestaurant restaurants={this.state.restaurants} bounds={this.state.restaurantBounds} lat={this.state.lat} lon={this.state.lon} />
         }
+
+
+
+        {!this.state.loading && this.state.submitRestaurant && !this.state.submitShelter && < div style={{ height: '100%', width: '50%', float: 'left' }}>
+          <div>
+            <strong>List of shelters near you:</strong>
+          </div>
+          <form onSubmit={this.onSubmitShelter}>
+            {Object.keys(this.state.shelters).map(key => (
+
+
+              <label key={key} value={key}>
+                <input type="radio" value={key} checked={this.state.selectedOption === key} onChange={this.handleOptionChangeShelter} />
+                <div>
+                  <strong>Name:</strong> {this.state.shelters[key].Name}
+                </div>
+                <div>
+                  <strong>Address:</strong> {this.state.shelters[key].Address}
+                </div>
+                <div>
+                  <strong>Phone:</strong> {this.state.shelters[key]['Phone Number']}
+                </div>
+              </label>
+
+            ))}
+
+            <button className="btn btn-primary" style={{ right: "52%", position: "absolute", top: "13%" }} type="submit">
+              Select
+                </button>
+          </form>
+        </div>
+        }
+        {
+          !this.state.loading && this.state.submitRestaurant && !this.state.submitShelter && <GoogleApiShelter shelters={this.state.shelters} bounds={this.state.shelterBounds} lat={this.state.lat} lon={this.state.lon} />
+        }
+
+        {this.state.submitRestaurant && this.state.submitShelter && < div style={{ height: '100%', width: '50%', float: 'left' }}>
+        <Results restaurantKey={this.state.chosenRestaurant} restaurants={this.state.restaurants} shelterKey={this.state.chosenShelter} shelters={this.state.shelters} />
+        </div>
+        }
+        {
+          this.state.submitRestaurant && this.state.submitShelter && <GoogleApiResult lat={this.state.lat} lon={this.state.lon} restaurantLat={this.state.restaurants[this.state.chosenRestaurant].coordinates.latitude} 
+          restaurantLon={this.state.restaurants[this.state.chosenRestaurant].coordinates.longitude} 
+          shelterLat={this.state.shelters[this.state.chosenShelter].Coordinates.Latitude}
+          shelterLon={this.state.shelters[this.state.chosenShelter].Coordinates.Longitude}></GoogleApiResult>
+        }
+
+        
       </div >
     )
   }
 }
 
-
-class GoogleMapRestaurant extends Component {
-
+class ResultGoogleMap extends Component {
   constructor(props) {
-    let bounds = new props.google.maps.LatLngBounds();
-    for (let i = 0; i < props.coordinates; i++) {
-      bounds.extend(props.coordinates[i]);
-    }
+    let bounds = new props.google.maps.LatLngBounds();;
+    bounds.extend({
+      lat: props.lat, lng: props.lon
+    })
+    bounds.extend({
+      lat: props.shelterLat, lng: props.shelterLon
+    })
+    bounds.extend({
+      lat: props.restaurantLat, lng: props.restaurantLon
+    })
     super(props)
     this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
       bounds: bounds
-    };
-  }
-
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
     }
-  };
+  }
 
   render() {
     return (
@@ -181,19 +229,154 @@ class GoogleMapRestaurant extends Component {
           bounds={this.state.bounds}
         >
           < Marker
-            onClick={this.onMarkerClick}
             name={'Current Location'}
             position={{ lat: this.props.lat, lng: this.props.lon }}
-          />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-          >
-            <div>
-              <h6>{this.state.selectedPlace.name}</h6>
-            </div>
-          </InfoWindow>
+          ></Marker>
+
+
+          < Marker
+            name={'Chosen Restaurant'}
+            position={{ lat: this.props.restaurantLat, lng: this.props.restaurantLon }}
+          ></Marker>
+
+          < Marker
+            name={'Chosen Shelter'}
+            position={{ lat: this.props.shelterLat, lng: this.props.shelterLon }}
+          ></Marker>
+        </Map>
+      </div>
+    )
+  }
+}
+
+class Results extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <strong>Chosen Restaurant:</strong>
+          <div>
+            Name: {this.props.restaurants[this.props.restaurantKey].name}
+          </div>
+          <div>
+            Address: {this.props.restaurants[this.props.restaurantKey].address}
+          </div>
+          <div>
+            Phone: {this.props.restaurants[this.props.restaurantKey].phone}
+          </div>
+        </div>
+        <div>
+          <strong>Chosen Shelter:</strong>
+          <div>
+            Name: {this.props.shelters[this.props.shelterKey].Name}
+          </div>
+          <div>
+            Address: {this.props.shelters[this.props.shelterKey].Address}
+          </div>
+          <div>
+            Phone: {this.props.shelters[this.props.shelterKey]['Phone Number']}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class GoogleMapRestaurant extends Component {
+
+  constructor(props) {
+    let bounds = new props.google.maps.LatLngBounds();
+    for (let i = 0; i < props.coordinates; i++) {
+      bounds.extend(props.coordinates[i]);
+    }
+    super(props)
+    this.state = {
+      bounds: bounds
+    };
+  }
+
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '50%', float: "right" }}>
+        <Map
+          google={this.props.google}
+          style={
+            {
+              width: '50%',
+              height: '100vh'
+            }
+          }
+          initialCenter={{
+            lat: this.props.lat,
+            lng: this.props.lon
+          }}
+          bounds={this.state.bounds}
+        >
+          < Marker
+            name={'Current Location'}
+            position={{ lat: this.props.lat, lng: this.props.lon }}
+          ></Marker>
+
+          {
+            Object.keys(this.props.restaurants).map(key => (
+              <Marker key = {key}
+                name={this.props.restaurants[key].name}
+                position={{ lat: this.props.restaurants[key].coordinates.latitude, lng: this.props.restaurants[key].coordinates.longitude }}
+              ></Marker>
+            ))
+          }
+
+        </Map>
+      </div>
+    )
+  }
+}
+
+
+
+class GoogleMapShelter extends Component {
+
+  constructor(props) {
+    let bounds = new props.google.maps.LatLngBounds();
+    for (let i = 0; i < props.coordinates; i++) {
+      bounds.extend(props.coordinates[i]);
+    }
+    super(props)
+    this.state = {
+      bounds: bounds
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '50%', float: "right" }}>
+        <Map
+          google={this.props.google}
+          style={
+            {
+              width: '50%',
+              height: '100vh'
+            }
+          }
+          initialCenter={{
+            lat: this.props.lat,
+            lng: this.props.lon
+          }}
+          bounds={this.state.bounds}
+        >
+          < Marker
+            name={'Current Location'}
+            position={{ lat: this.props.lat, lng: this.props.lon }}
+          ></Marker>
+          {
+            Object.keys(this.props.shelters).map(key => (
+              <Marker key={key}
+                name={this.props.shelters[key].name}
+                position={{ lat: this.props.shelters[key].Coordinates.Latitude, lng: this.props.shelters[key].Coordinates.Longitude }}
+              ></Marker>
+            ))
+          }
         </Map>
       </div>
     )
@@ -219,6 +402,17 @@ const HomePage = () => (
     }
   </FirebaseContext.Consumer>
 );
+
+const GoogleApiShelter = GoogleApiWrapper(
+  {
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+  })(GoogleMapShelter)
+
+
+  const GoogleApiResult = GoogleApiWrapper(
+    {
+      apiKey: process.env.REACT_APP_GOOGLE_API_KEY
+    })(ResultGoogleMap)
 
 
 
